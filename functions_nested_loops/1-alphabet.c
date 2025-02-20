c5825199b498d9567586326ffcb41c8674d6777d
Jackpot-007 <jackpotmerlon@gmail.com>
2025-02-20 23:34:38 +0000

    Estetica 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Imprime el alfabeto en minúsculas
+ * seguido de una nueva línea.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
+}
