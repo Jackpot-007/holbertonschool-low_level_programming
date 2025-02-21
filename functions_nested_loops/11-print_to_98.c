03420e8668dc606d8350249d96051fd91dd5b799
Jackpot-007 <jackpotmerlon@gmail.com>
2025-02-21 20:30:55 +0000

    Estetica 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+
+	if (n / 10)
+	print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: Starting number
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+	print_number(n);
+	_putchar(',');
+	_putchar(' ');
+
+	if (n < 98)
+	n++;
+	else
+	n--;
+	}
+	print_number(98);
+	_putchar('\n');
+}
