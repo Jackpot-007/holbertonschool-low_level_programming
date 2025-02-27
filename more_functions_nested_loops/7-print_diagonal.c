c9be9dd0e5a917ad0e7065f5b98fa2e7f6092c70
Jackpot-007 <jackpotmerlon@gmail.com>
2025-02-27 13:03:41 +0000

    Arreglo 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal.
+ * @n: The number of times the '\' character should be printed.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+	_putchar('\n');  /* Print only a new line if n is 0 or less */
+	return;
+	}
+
+	for (i = 0; i < n; i++)  /* Loop to print n lines */
+	{
+	for (j = 0; j < i; j++)  /* Print spaces before '\' */
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');  /* Print '\' */
+	_putchar('\n');  /* Move to the next line */
+	}
+}
