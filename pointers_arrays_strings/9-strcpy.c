f926c76c110daf75c73aafe5c9f661c873100233
Jackpot-007 <jackpotmerlon@gmail.com>
2025-03-01 03:56:26 +0000

    Esencia 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src, including the terminating null byte.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	/* Copy each character from src to dest */
+	while (src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	/* Add the null terminator to dest */
+	dest[i] = '\0';
+
+	return dest;
+}
