e351f081a875f784c5d7378803268b98ef51795b
Jackpot-007 <jackpotmerlon@gmail.com>
2025-03-02 16:25:09 +0000

    Brazo de 35 XD 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: 0 if equal, a positive value if s1 > s2,
+ * a negative value if s1 < s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
