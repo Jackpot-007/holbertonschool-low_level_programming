804bfc9aaa70969c973162ab0c37a62b2ed509ce
Jackpot-007 <jackpotmerlon@gmail.com>
2025-03-17 16:25:12 +0000

    structures_typedef 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * _strlen - helper function
+ * @s: pointer
+ * Return: return len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strcpy - helper function
+ * @dest: pointer
+ * @src: pointer
+ * Return: returns fromst
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
+ * new_dog - Creates a new dog t
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: return new d
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+	char *n_copy, *o_copy;
+	int n_len, o_len;
+
+	if (!name || !owner)
+	{
+		return (NULL);
+	}
+	n_len = _strlen(name) + 1;
+	o_len = _strlen(owner) + 1;
+	new_d = malloc(sizeof(dog_t));
+	if (!new_d)
+	{
+		return (NULL);
+	}
+	n_copy = malloc(n_len);
+	if (!n_copy)
+	{
+		free(new_d);
+		return (NULL);
+	}
+	_strcpy(n_copy, name);
+	o_copy = malloc(o_len);
+	if (!o_copy)
+	{
+		free(n_copy);
+		free(new_d);
+		return (NULL);
+	}
+	_strcpy(o_copy, owner);
+	new_d->name = n_copy;
+	new_d->age = age;
+	new_d->owner = o_copy;
+
+	return (new_d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,17 +7,13 @@
  * @age: edad del perro
  * @owner: dueÃo del perro
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
 #endif /* DOG_H */
