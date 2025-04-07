b2596e64011caa05c53df737253d49697dc74c88
Jackpot-007 <jackpotmerlon@gmail.com>
2025-04-07 00:01:44 +0000

    singly_linked_lists 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - Libera todos los nodos de una lista vinculada
+ * @head: Puntero a la cabecera de la lista vinculada
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head, *next;
+
+	if (head == NULL)
+		return;
+
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = next;
+	}
+}
