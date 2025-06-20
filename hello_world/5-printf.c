4bd3cbc25b2de841a3338c2cac47a652036f9b14
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-20 23:00:58 +0000

    pizzitaÃ

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada
+ * Return: Siempre 0 (Exito)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
