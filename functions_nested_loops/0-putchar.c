e09697fbf49cf008d9be4e017f91f285dd6a02fc
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-24 15:48:35 +0000

    hhudf

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include "_putchar.c"
 #include "main.h"
 
 /**
- * main - putchar prints "_putchar"
+ * main - putchar prints "wiwi"
  *
  * Return: Always (0)
  */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
