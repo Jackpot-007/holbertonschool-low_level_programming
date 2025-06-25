e7afb2efaf4206f75f4132d1c557782e19a26426
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-25 04:18:39 +0000

    sdasda

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print function that prints the alphabet 10
+ * times, in lowercase.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
+	}
+	_putchar('\n');
+	}
+	i++;
+}
