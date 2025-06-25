92a7fa93dd709b0b4b583ec11077f6fbcf78614a
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-25 15:59:54 +0000

    dfsdf

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print function that prints the alphabet 10
+ * print_alphabet_x10 - print function that prints the alphabet
  * times, in lowercase.
  *
  * Return: void
@@ -12,13 +12,13 @@ void print_alphabet_x10(void)
 	char j;
 	int i = 0;
 
-	while (i <= 0)
+	while (i < 10)
 	{
 	for (j = 'a'; j <= 'z'; j++)
 	{
 	_putchar(j);
 	}
 	_putchar('\n');
-	}
 	i++;
+	}
 }
