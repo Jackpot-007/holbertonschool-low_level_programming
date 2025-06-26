d1ec1d262c4ef1cad23805b8018757546d9b812b
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-26 17:54:15 +0000

    nuhuiohiuo

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - this checks lowercase characters.
+ *
+ * Return: 0 and 1
+ */
+
+int _islower(int c)
+{
+	char ch1 = 'a';
+	char ch2 = 'B';
+
+	if (islower(ch1))
+	{
+	
