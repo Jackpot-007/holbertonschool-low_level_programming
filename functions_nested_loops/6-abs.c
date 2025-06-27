c6b645587962daf22100521625744ee6d90cc468
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-27 19:19:51 +0000

    fddf

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - this calculates the absolute value of an integer.
+ *
+ * @x: wawawawawa
+ *
+ * Return: Always 0 (success)
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
