ae520d8545b30cfbc932fa232bf6b197f702386e
Jackpot-007 <jackpotmerlon@gmail.com>
2025-06-30 03:41:10 +0000

    sdfsdf

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this makes it add two integers and returns the result
+ *
+ * @n1: number 1
+ *
+ * @n2: number 2
+ *
+ * Return: n1 + n2
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
