5fa4eb9abc19f3c450f9b19ed98ee9d287ba3f23
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-03 18:05:27 +0000

    toooodoooo

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
+ * int _isupper - check uppercase characters
  *
+ * @c: verified character
+ *
+ * Return: 1, c and 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
