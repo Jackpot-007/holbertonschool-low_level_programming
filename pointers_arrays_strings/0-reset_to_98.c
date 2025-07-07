e153e8fdee1ca483ca47286b4ca7bd1538b423cd
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-07 02:49:19 +0000

    sdfsdf

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter
+ *
+ * @n: updates the value it points to to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
