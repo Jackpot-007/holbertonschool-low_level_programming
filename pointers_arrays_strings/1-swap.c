d59c6ddbe887778b84de710f88b9e06ce7be76a0
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-07 03:01:36 +0000

    sadasd

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - exchange the values of two integers.
+ *
+ * @a: character of pointer a.
+ *
+ * @b: character of pointer b.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
