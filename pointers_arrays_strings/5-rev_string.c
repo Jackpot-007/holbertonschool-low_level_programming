640b4c8f09a6d65e94a86bdff778185d005cb2f4
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-07 05:03:06 +0000

    sadasd

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
  * rev_string - reverse a string
@@ -12,10 +13,15 @@ void rev_string(char *s)
 	int length = strlen(s);
 	int i;
 	char temp;
+
+	printf("Original: %s\n", s);
+
 	for (i = 0; i < length / 2; i++)
 	{
-		temp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
+	temp = s[i];
+	s[i] = s[length - i - 1];
+	s[length - i - 1] = temp;
 	}
+
+	printf("Invertida: %s\n", s);
 }
