ca8979c0bed6d2561f25c98318cc1a070b8d9366
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-10 04:37:01 +0000

    asdfas

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * puts2 - prints every other character of a string, starting
@@ -10,14 +10,34 @@
 
 void puts2(char *s)
 {
-	int a;
+	int length = 0;
+	int i;
 
-	for (a = 0; s[a] != '\0'; a++)
+	if (s == NULL)
 	{
-		if (a % 2 == 0)
-		{
-			printf("%c", s[a]);
-		}
+		char nl = '\n';
+		write(1, &nl, 1);
+		return;
 	}
-	printf("\n");
+
+	length = strlen(s);int length = 0;
+	int i;
+
+	if (s == NULL)
+	{
+			char nl = '\n';
+			write(1, &nl, 1);
+			return;
+	}
+
+	length = strlen(s);
+
+	for (i = length - 1; i >= 0; i--)
+	{
+			write(1, &s[i], 1);
+	}
+	{
+			char nl = '\n';
+			write(1, &nl, 1);
+	}
 }
