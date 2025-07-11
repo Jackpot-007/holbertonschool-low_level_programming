c52d6b64af95778857acfe340a6a9646a7c1c405
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-11 19:20:56 +0000

    dfsdf

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - fills memory with a constant byte
+ *
+ * @s, b, n: pointer character
+ *
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	
