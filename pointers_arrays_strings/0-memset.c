b8ae4c2e4b2b8024e633bdea7b9ba322fb0c0c76
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-12 00:18:34 +0000

    sadasd

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,11 +4,20 @@
 /**
  * *_memset - fills memory with a constant byte
  *
- * @s, b, n: pointer character
+ * @s: pointer character
+ *
+ * @b: constant byte
+ *
+ * @n: number of bytes
  *
  * Return: pointer s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
