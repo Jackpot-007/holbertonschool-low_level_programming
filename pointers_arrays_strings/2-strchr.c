c05cd571c5e44480ec9bff8fee8749862846903d
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-15 14:03:30 +0000

    dfsdf

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,12 @@
 char *_strchr(char *s, char c)
 {
 	if (s == NULL)
+	
 		return (NULL);
 
-	for(; *s != '\0'; s++)
-		if(*s == c)
-			return s;
-	return (NULL);
+	if (*s == c)
+	{
+		return (s);
+	}
+		return (0);
 }
