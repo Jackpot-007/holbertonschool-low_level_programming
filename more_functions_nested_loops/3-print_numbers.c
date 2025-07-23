a189f9a5e7eb481e654e45a8747530e371307a51
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-23 14:40:49 +0000

    aaña

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
  */
 
 void print_numbers(void)
