0a9042243462d43baf705e96a6c425480f3d4329
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-23 14:22:33 +0000

    sdad

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -10,4 +10,9 @@
 
 int _isdigit(int c)
 {
-	in
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ *
+ * @a: character a
+ *
+ * @b: character b
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
