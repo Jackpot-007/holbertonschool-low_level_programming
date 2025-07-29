f09966a4e00a17f1271f05a479da50b4781af16a
Jackpot-007 <jackpotmerlon@gmail.com>
2025-07-29 15:01:22 +0000

    fgfdgdfg

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ *
+ * @n: character integer.
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
