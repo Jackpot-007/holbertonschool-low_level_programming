466c1d4f508e1acc1c93a1b1b7c16a7fb41834a6
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-04 19:13:17 +0000

    sdadas

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ *
+ * @dest: character pointer
+ *
+ * @src: character pointer
+ *
+ * @n: integer pointer
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, src_len = 0;
+
+	while (src[i++])
+		src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -6,13 +6,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
+	char str[] = "Look up!\n";
+	char *ptr;
 
-	t = _strstr(s, f);
-	printf("%s\n", t);
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
 }
